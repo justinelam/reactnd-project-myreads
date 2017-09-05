ed2e825346dbf80310db9c389c95723a67e2faa5
Justine Lam <justine@shareprogress.org>
2017-09-04 18:55:05 -0700

    Clean up code from final review comments - check for book.author before displaying them joined with , - move CSS to stylesheet

--- src/Book.js
+++ src/Book.js
@@ -19,7 +19,7 @@ class Book extends Component {
 		return(
 			<div className="book">
 			  <div className="book-top">
-			    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
+			    <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
 			    <div className="book-shelf-changer">
 			      <select value={shelf} onChange={(e)=>changeStatus(book, e.target.value)}>
 			        <option value="" disabled>Move to...</option>
@@ -31,7 +31,7 @@ class Book extends Component {
 			    </div>
 			  </div>
 			  <div className="book-title">{book.title}</div>
-			  <div className="book-authors">{book.authors}</div>
+			  <div className="book-authors">{book.authors ? book.authors.join(', '): ''}</div>
 			</div>
 		)
 	}
